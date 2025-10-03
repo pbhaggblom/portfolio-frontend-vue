42e2e8020aea2a25b690592837d6bbfc093a6ccc
Pontus Häggblom <pbhaggblom@gmail.com>
2025-10-03 11:47:28 +0000

    Adds navbar functionality

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import About from './About.vue';
+    import Projects from './Projects.vue';
+    import Contact from './Contact.vue';
+
+    const emit = defineEmits(['update'])
+
+    const setActive = (component) => {
+        emit('update', component)
+    }
+
+</script>
+
+<template>
+    <nav>
+        <button @click="setActive(About)">About</button>
+        <button @click="setActive(Projects)">Projects</button>
+        <button @click="setActive(Contact)">Contact</button>
+    </nav>
+</template>
