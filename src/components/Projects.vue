7eb2ea25030b4778d64e0da39f3fa47db76a73d6
Pontus Häggblom <pbhaggblom@gmail.com>
2025-10-04 09:50:16 +0000

    Renders project descriptions from array

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,21 @@
-<script setup></script>
+<script setup>
+    const projects = [
+        {
+            title: 'Frontend',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione rem voluptate harum nobis libero nesciunt totam consequatur officiis nisi asperiores?'
+        },
+        {
+            title: 'Backend',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, officia quae. Quasi id deserunt deleniti delectus voluptatem consequatur. Maiores, eaque!'
+        }
+    ];
+</script>
 
 <template>
-    <p>projects</p>
+    <div v-for="project in projects">
+        <div>
+            <h3>{{ project.title }}</h3>
+            <p>{{ project.description }}</p>
+        </div>
+    </div>
 </template>
